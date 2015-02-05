427216cff207e4e005a17e40fdc124dac6fdec6b
Dmitri Koulikoff <dima@koulikoff.ru>
2015-02-06 01:02:56 +0300

    Rspecs for Customer added

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,6 @@
 class Customer < ActiveRecord::Base
+
   validates :name, presence: true
   validates :email, presence: true
+
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,4 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Customer, :type => :model do
+
+  subject {create :customer}
+
+  it {should be_valid}
+  it {should validate_presence_of :name}
+  it {should validate_presence_of :email}
+
 end
