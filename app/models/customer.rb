b07ef8eb532d131f8ca9d23167e7e34667005e7e
Dmitri Koulikoff <dima@koulikoff.ru>
2015-02-06 03:31:25 +0300

    Customer is saved to Stripe

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,28 @@ class Customer < ActiveRecord::Base
   validates :name, presence: true
   validates :email, presence: true
 
+  before_create :add_to_stripe
+  before_destroy :remove_from_stripe
+  after_rollback :remove_from_stripe
+
+  def stripe_object
+    @stripe_object ||= Stripe::Customer.retrieve stripe_id
+  end
+
+  private
+
+  def add_to_stripe
+    @stripe_object = Stripe::Customer.create email: email, metadata: {name: name}
+    self.stripe_id = @stripe_object.id
+  rescue
+    # TODO: add error to #errors
+    false
+  end
+
+  def remove_from_stripe
+    stripe_object.delete
+  rescue
+    true
+  end
+
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -8,4 +8,43 @@ RSpec.describe Customer, :type => :model do
   it {should validate_presence_of :name}
   it {should validate_presence_of :email}
 
+  describe 'on Stripe server' do
+    context 'at creation' do
+      it 'adds a new Customer on Stripe' do
+        expect {create :customer}.to change {Stripe::Customer.all(include: ['total_count'])['total_count']}.by(1)
+      end
+
+      it 'assigns to the newly created customer the stripe_id' do
+        expect(subject.stripe_id).to eq Stripe::Customer.all(limit: 1).first.id
+      end
+
+      it 'assigns the Stripe object to #stripe_object' do
+        expect(subject.stripe_object.class).to be Stripe::Customer
+        expect(subject.stripe_object.to_hash).to eq Stripe::Customer.all(limit: 1).first.to_hash
+      end
+
+      it "assigns name to the stripe object's metadata" do
+        expect(subject.stripe_object['metadata']['name']).to eq subject.name
+      end
+
+      it 'assigns email to the stripe object' do
+        expect(subject.stripe_object['email']).to eq subject.email
+      end
+    end
+
+    context 'at delettion' do
+
+      it 'removes the Customer from Stripe' do
+        subject
+        expect {subject.destroy}.to change {Stripe::Customer.all(include: ['total_count'])['total_count']}.by(-1)
+      end
+    end
+  end   # describe 'on Stripe server'
+
+  describe '#stripe_object' do
+    it 'fetches the corresponding Stripe::Customer' do
+      expect(Customer.find(subject.id).stripe_object).to be
+    end
+  end
+
 end
